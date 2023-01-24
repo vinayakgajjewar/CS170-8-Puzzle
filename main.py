3be0f98b016e39ef71e7d9bf1f47d303ddd25987
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-01-24 14:15:01 -0800

    create necessary files

---
+++ main.py
@@ -0,0 +1,10 @@
+import puzzle
+import solver
+
+# TODO: We want the user to be able to enter an arbitrary initial state
+# For now, we will hardcode an arbitrary initial state
+# We will use -1 to represent the blank space
+row1 = [1, 2, 3]
+row2 = [4, 5, 6]
+row3 = [7, -1, 8]
+initial_state = [row1, row2, row3]
