fb9b27f3deff55a6fe3e95db423983e8d00e45a7
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-01-25 15:07:53 -0800

    let the user choose their choice of algorithm

--- main.py
+++ main.py
@@ -15,14 +15,26 @@ eight_puzzle = problem.Problem(8)
 my_solver = solver.Solver()
 
 print('Welcome to vgajj002\'s 8-puzzle solver.')
+
+# Choose whether to use the default or a custom initial state
 print('Type \'1\' to use a default puzzle or type \'2\' to enter your own puzzle.')
-response = input()
-if response == '1':
+initial_state_response = input()
+if initial_state_response == '1':
     pass
-elif response == '2':
+elif initial_state_response == '2':
     print('Enter your puzzle, using a 0 to represent the blank.')
-    print('Enter the first row, using spaces or tabs between numbers: ')
-    print('Enter the second row, using spaces or tabs between numbers: ')
-    print('Enter the third row, using spaces or tabs between numbers: ')
+    print('Enter the first row, using spaces or tabs between numbers: ', end='')
+    first_row_input = input()
+    print('Enter the second row, using spaces or tabs between numbers: ', end='')
+    second_row_input = input()
+    print('Enter the third row, using spaces or tabs between numbers: ', end='')
+    third_row_input = input()
 else:
     sys.exit('Invalid response. Goodbye!')
+
+# Get choice of algorithm from user
+print('Enter your choice of algorithm.')
+print('1. Uniform cost search (h(n) = 0).')
+print('2. A* with misplaced tile heuristic.')
+print('3. A* with euclidean distance heuristic.')
+algorithm_choice = input()
