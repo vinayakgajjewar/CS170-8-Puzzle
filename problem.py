7e1a1bbd9b1d8eb1c5bb868c2c1f0d638ea1663b
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-01-25 14:28:00 -0800

    rename puzzle to problem

---
+++ problem.py
@@ -0,0 +1,16 @@
+import sys
+
+class Problem:
+
+    initial_state = ""
+
+    goal_state = ""
+
+    operators = ""
+
+    def __init__(self, size):
+        #print(f'Initializing problem with size {size}')
+        self.size = size
+
+if __name__ == '__main__':
+    sys.exit('Don\'t run problem.py directly!')
