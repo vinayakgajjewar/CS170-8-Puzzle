9e51749bac824738231a97e5d97386f9a50433fd
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-01-25 14:27:13 -0800

    create node and tree classes

---
+++ node.py
@@ -0,0 +1,9 @@
+import sys
+
+class Node:
+
+    # Pointer to parent node
+    parent = None
+
+if __name__ == '__main__':
+    sys.exit('Don\'t run node.py directly!')
---
+++ tree.py
@@ -0,0 +1,7 @@
+import sys
+
+class Tree:
+    pass
+
+if __name__ == '__main__':
+    sys.exit('Don\'t run tree.py directly!')
