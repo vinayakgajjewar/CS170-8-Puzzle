d88f8f865785968b07ebe453ff86c5f998b8d9e6
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-01-25 16:07:07 -0800

    change representation of init state, goal state

--- problem.py
+++ problem.py
@@ -2,11 +2,17 @@ import sys
 
 class Problem:
 
-    initial_state = ""
+    # Initial state
+    initial_row_1 = []
+    initial_row_2 = []
+    initial_row_3 = []
 
-    goal_state = ""
+    # Goal state
+    goal_row_1 = [1, 2, 3]
+    goal_row_2 = [4, 5, 6]
+    goal_row_3 = [7, 8, 0]
 
-    operators = ""
+    operators = []
 
     def __init__(self, size):
         #print(f'Initializing problem with size {size}')
