68a6ed6398a62a77784b7a32bc78a8d991c848fb
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-02-01 14:38:07 -0800

    add stuff to test script

--- test_node.py
+++ test_node.py
@@ -14,4 +14,15 @@ print(f'The tile at [2, 1] is {n1.get_tile(2, 1)}')
 
 new_rows = n1.blank_up()
 n2 = node.Node(new_rows[0], new_rows[1], new_rows[2])
+print('After moving the blank up:')
 print(n2)
+
+new_rows = n2.blank_up()
+n3 = node.Node(new_rows[0], new_rows[1], new_rows[2])
+print('After moving the blank up again:')
+print(n3)
+
+new_rows = n3.blank_down()
+n4 = node.Node(new_rows[0], new_rows[1], new_rows[2])
+print('After moving the blank down:')
+print(n4)
