50148c2e81a6569256698b52dcb2166e90db3369
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-01-31 18:42:04 -0800

    added pseudocode for graph search alg

--- solver.py
+++ solver.py
@@ -1,13 +1,35 @@
 import sys
+import heapq
 
 class Solver:
     def __init__(self):
         #print('Initializing solver')
         pass
 
-        # This function will run graph search on the 
+    # This function will run graph search on the problem passed into it
     def graph_search(self, problem):
-        pass
+
+        # TODO: initialize frontier using initial state of problem
+        frontier = []
+        heapq.heapify(frontier)
+
+        # Initialize empty explored set
+        explored_set = []
+
+        while True:
+
+            # If the frontier is empty, return a failure
+            if len(frontier) == 0:
+                print("Could not find a solution.")
+            
+            # Pick a leaf node and remove it from the frontier
+
+            # If the node contains a goal state, return the corresponding solution
+
+            # Add the node to the explored set
+
+            # Expand the chosen node and add the resulting nodes to the frontier
+            # Only if they're not already in the frontier or the explored set
 
 if __name__ == '__main__':
     sys.exit('Don\'t run solver.py directly!')
