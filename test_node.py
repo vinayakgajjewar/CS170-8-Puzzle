1bd46da5242152663e155f6e7a63e4910191de18
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-02-01 16:14:50 -0800

    finish test script

--- test_node.py
+++ test_node.py
@@ -9,8 +9,6 @@ row3 = [7, 8, 0]
 
 n1 = node.Node(row1, row2, row3)
 print(n1)
-print(f'Tile 2 is located at {n1.locate_tile(2)}') # Should be [1, 2]
-print(f'The tile at [2, 1] is {n1.get_tile(2, 1)}')
 
 new_rows = n1.blank_up()
 n2 = node.Node(new_rows[0], new_rows[1], new_rows[2])
@@ -22,7 +20,32 @@ n3 = node.Node(new_rows[0], new_rows[1], new_rows[2])
 print('After moving the blank up again:')
 print(n3)
 
-new_rows = n3.blank_down()
+new_rows = n3.blank_left()
 n4 = node.Node(new_rows[0], new_rows[1], new_rows[2])
-print('After moving the blank down:')
+print('After moving the blank left:')
 print(n4)
+
+new_rows = n4.blank_left()
+n5 = node.Node(new_rows[0], new_rows[1], new_rows[2])
+print('After moving the blank left again:')
+print(n5)
+
+new_rows = n5.blank_down()
+n6 = node.Node(new_rows[0], new_rows[1], new_rows[2])
+print('After moving the blank down:')
+print(n6)
+
+new_rows = n6.blank_down()
+n7 = node.Node(new_rows[0], new_rows[1], new_rows[2])
+print('After moving the blank down again:')
+print(n7)
+
+new_rows = n7.blank_right()
+n8 = node.Node(new_rows[0], new_rows[1], new_rows[2])
+print('After moving the blank right:')
+print(n8)
+
+new_rows = n8.blank_right()
+n9 = node.Node(new_rows[0], new_rows[1], new_rows[2])
+print('After moving the blank right again:')
+print(n9)
