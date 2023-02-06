c21d1b79079d4fd7063f003748515c0b00f6a991
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-02-05 16:13:51 -0800

    do some work on tree.py

--- tree.py
+++ tree.py
@@ -1,7 +1,54 @@
 import sys
+import heapq
+import node
 
 class Tree:
-    pass
+
+    # Explored set
+    explored_set = None
+
+    # Frontier
+    # This will be implemented as a priority queue
+    frontier = []
+
+    # Head node
+    # This is the initial state of the problem
+    head = None
+
+    # Constructor
+    # initial_state is a list of rows representing the initial state of the problem
+    def __init__(self, initial_state):
+
+        # Create the initial node
+        init_row1 = initial_state[0]
+        init_row2 = initial_state[1]
+        init_row3 = initial_state[2]
+        head = node.Node(init_row1, init_row2, init_row3)
+
+        # Initialize the explored set to be empty
+        explored_set = []
+
+        # Initialize the frontier with the initial state of the problem
+        heapq.heapify(self.frontier)
+        heapq.heappush(self.frontier, head)
+
+    # Utility method to check if the frontier is empty
+    # Return true if the frontier is empty
+    # Return false if there are nodes in the frontier
+    def is_frontier_empty(self):
+        if len(self.frontier) == 0:
+            return True
+        else:
+            return False
+    
+    # Pop a leaf node from the frontier
+    def pop_leaf_node(self):
+        return heapq.heappop(self.frontier)
+
+    # Add a node to the explored set
+    def add_to_explored(self, node):
+        self.frontier.append(node)
+
 
 if __name__ == '__main__':
     sys.exit('Don\'t run tree.py directly!')
